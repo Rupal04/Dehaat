ca771b39929a2ff926c67fb9228f86f44674a1d8
rupal04 <rupalgupta040895@gmail.com>
2020-05-04 00:41:54 +0530

    Added table name in models

--- balancesheet/models.py
+++ balancesheet/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 
 class BalSheet(models.Model):
+    class Meta(object):
+        db_table = 'balsheet'
     particular = models.CharField(max_length=800, blank=False, null=False)
     year_2015 = models.CharField(max_length=200)
     year_2016 = models.CharField(max_length=200)
